f897cdd3a25d6294726c13ae3157a5882bd5e8ef
sourab-jadav <sourabhjadav7@gmail.com>
2023-10-29 15:17:14 +0530

    theme toggle

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,7 +1,24 @@
 function ColorMyPencils(color)
-	color=color or "rose-pine"
-	vim.cmd.colorscheme(color)
-	vim.api.nvim_set_hl(0,"Normal",{bg="none"})
-	vim.api.nvim_set_hl(0,"NormalFloat",{bg="none"})
+    color = color or "rose-pine"
+    vim.cmd.colorscheme(color)
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
-ColorMyPencils()
+
+-- Define the two color schemes
+vim.cmd('colorscheme gruvbox')
+local current_colorscheme = 'gruvbox'
+
+-- Function to toggle between the two color schemes
+function ToggleColorscheme()
+    if current_colorscheme == 'rose-pine' then
+        vim.cmd('colorscheme gruvbox')
+        current_colorscheme = 'gruvbox'
+    else
+        ColorMyPencils()
+        current_colorscheme = 'rose-pine'
+    end
+end
+
+-- Map a key combination to toggle the color scheme
+vim.api.nvim_set_keymap('n', '<F12>', [[:lua ToggleColorscheme()<CR>]], { noremap = true, silent = true })
