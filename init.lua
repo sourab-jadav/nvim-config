1cd6a71e938725a6a5ab713741bc14eb000b21ab
sourab-jadav <sourabhjadav7@gmail.com>
2023-12-19 23:00:04 +0530

    auto command for folding

--- init.lua
+++ init.lua
@@ -4,3 +4,10 @@ require('sourab.set');
 require('sourab.snippets');
 --vim.cmd('colorscheme desert')
 
+-- autoloading views
+vim.api.nvim_exec([[
+  autocmd BufWinLeave *.cpp mkview
+  autocmd BufWinEnter *.cpp silent! loadview
+]], false)
+
+vim.api.nvim_exec([[:highlight Folded guibg=grey guifg=blue]], false)
