54bf145cbadd08911265f9171190647d3badee8b
sourab-jadav <sourabhjadav7@gmail.com>
2023-12-19 22:56:04 +0530

    toggling the jk shortcut

---
+++ after/plugin/togglejk.lua
@@ -0,0 +1,25 @@
+-- Initialize a variable to track the mapping state
+local mapping_enabled = false
+
+-- Function to toggle the mapping
+function toggle_mapping()
+    if mapping_enabled then
+        -- Remove the mapping
+        -- Execute :iunmap commands
+        vim.cmd('iunmap jk')
+        vim.cmd('iunmap kj')
+        print("Mapping disabled")
+    else
+        -- Set the mapping
+        vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('i', 'kj', '<Esc>', { noremap = true, silent = true })
+        print("Mapping enabled")
+    end
+
+    -- Update the state
+    mapping_enabled = not mapping_enabled
+end
+
+-- Example: Map <Leader>t to toggle the mapping
+vim.api.nvim_set_keymap('n', '<Leader>s', '<Cmd>lua toggle_mapping()<CR>', { noremap = true, silent = true })
+
