e5965011d0bce54980b357d2f7203c9db66b03b0
sourabh jadav <sourabhjadav7@gmail.com>
2024-07-08 10:00:53 +0530

    blaahh

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -22,5 +22,5 @@ require('telescope').setup{
 }
 vim.api.nvim_set_keymap('n', '<leader>tb', [[:Telescope buffers<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>of', [[:Telescope oldfiles<CR>]], { noremap = true, silent = true })
-
+-- looking at recently used files that belong to the pwd
 vim.api.nvim_set_keymap('n', '<leader>ost', "<cmd>lua require('telescope.builtin').oldfiles({ cwd_only = true })<cr>", { noremap = true, silent = true })
