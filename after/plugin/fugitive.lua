a23e4edd365413f5be846d303edfe39996279b3c
sourabh jadav <sourabhjadav7@gmail.com>
2024-07-08 09:58:24 +0530

    sdfsdf

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("n","<leader>gs",vim.cmd.Git);
-vim.keymap.set("n","<leader>gp",vim.cmd("Git push<CR>"));
+vim.keymap.set("n","<leader>gs",vim.cmd.Git); 
+vim.keymap.set("n","<leader>gp",vim.cmd("Git push<CR>")); -- you can use s shortcut to push the changes
